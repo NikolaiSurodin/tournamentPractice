23351b764c6d8adbf2d7f247347665c7df4a6d73
NikolaiSurodin <kolya_12.93@mail.ru>
2021-08-05 19:16:11 +0700

    first commit

---
+++ src/components/TournamentMatch.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TournamentMatch"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/view/TournamentTable.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TournamentTable"
+}
+</script>
+
+<style scoped>
+
+</style>
